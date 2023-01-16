5044c7fc4f3dc3ab45edba4b647026f247a4a256
sarthakgupta15 <sarthakgupta.univ@gmail.com>
2023-01-17 02:47:07 +0530

    Created 2_724 Find Pivot Index

---
+++ 2_ 724 Find Pivot Index.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution
+{
+    public:
+    int pivotIndex(vector<int>& nums)
+    {
+        int l=0, r=0;
+        for(int& v: i<nums.size(); ++i)
+        {
+            r-=nums[i];
+            
+        }
+
+    }
+};
+
+int main()
+{
+    
+    return 0;
+}
